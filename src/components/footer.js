68b8e13d51d42b71d641504c9169d2b9bb704eb1
Yousif Hibi <yousif.Hibi@gmail.com>
2025-07-13 15:24:08 +0300

    footer butons

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,32 +1,56 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './footer.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./footer.css";
 
-
-function Footer() { 
-    return (
-        <footer>
-            <div className='userName'>
-                    <h1>Yousif Hibi</h1>
-            </div>
-            <div className='redirectionButtons bottum'>
-                <div className='gitHub linkBtn'>
-                    <button><img src="/github-mark.png" width={30} height={30}></img></button>
-                </div>
-                <div className='linkedin linkBtn'>
-                    <button><img src="/linkedin.png" width={30} height={30}></img></button>
-                </div>
-                <div className='email linkBtn'>
-                    <button><img src="/gmail.png" width={30} height={30}></img></button>
-                </div>
-            </div>
-        </footer>
-
-
-    );
-   
-
-
+function Footer() {
+  return (
+    <footer>
+      <div className="userName">
+        <h1>Yousif Hibi</h1>
+      </div>
+      <div className="redirectionButtons bottum">
+        <div className="gitHub linkBtn">
+          <a
+            href="https://github.com/Yousif-Hibi"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <button>
+              <img
+                src="/github-mark.png"
+                width={30}
+                height={30}
+                alt="GitHub"
+              ></img>
+            </button>
+          </a>
+        </div>
+        <div className="linkedin linkBtn">
+          <a
+            href="https://www.linkedin.com/in/yousif-hibi/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <button>
+              <img
+                src="/linkedin.png"
+                width={30}
+                height={30}
+                alt="LinkedIn"
+              ></img>
+            </button>
+          </a>
+        </div>
+        <div className="email linkBtn">
+          <a href="mailto:yousif.hibi@gmail.com">
+            <button>
+              <img src="/gmail.png" width={30} height={30} alt="Email"></img>
+            </button>
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
