7386d4ed54a6927f3114d2d03392afb85d131f7f
Yousif Hibi <yousif.Hibi@gmail.com>
2025-07-21 12:46:03 +0300

    email

--- src/components/config.js
+++ src/components/config.js
@@ -1,7 +1,7 @@
 export default {
   emailjs: {
     serviceId: "service_def8ewj",
-    templateId: "template_uvcpoxq",
+    templateId: "template_ev8ppjn",
     publicKey: "8xBAugXy-QzYp_46H",
   },
 };
